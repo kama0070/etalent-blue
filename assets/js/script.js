aacea84b8ceeb1197f48ab107d8289db6e50ca19
Vikram Kamalakannan <43158970+kama0070@users.noreply.github.com>
2021-09-28 16:06:47 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+// All, Pre-Arrival, Post-Arrival - Show/Hide
+if($("#data-forecasts").css("display") != "none" && $("#analysis-reporting").css("display") != "none" && $("#smart-economy").css("display") != "none"){
+	$("#all-prog-tab").addClass("btn-rounded-white-select");
+}
+
+$(document).ready(function(){
+
+  $("#data-forecasts-tab").click(function(){
+	$("#data-forecasts").show();
+    $("#analysis-reporting").hide();
+	$("#smart-economy").hide();
+	// Add select class and remove it from others
+	$("#all-prog-tab").removeClass("btn-rounded-white-select");
+	$("#data-forecasts-tab").addClass("btn-rounded-white-select");
+	$("#analysis-reporting-tab").removeClass("btn-rounded-white-select");
+	$("#smart-economy-tab").removeClass("btn-rounded-white-select");
+  });
+
+  $("#analysis-reporting-tab").click(function(){
+    $("#data-forecasts").hide();
+    $("#analysis-reporting").show();
+	$("#smart-economy").hide();
+	// Add select class and remove it from others
+	$("#all-prog-tab").removeClass("btn-rounded-white-select");
+	$("#data-forecasts-tab").removeClass("btn-rounded-white-select");
+	$("#analysis-reporting-tab").addClass("btn-rounded-white-select");
+	$("#smart-economy-tab").removeClass("btn-rounded-white-select");
+  });
+  
+  $("#all-prog-tab").click(function(){
+    $("#data-forecasts").show();
+    $("#analysis-reporting").show();
+	$("#smart-economy").show();
+	// Add select class and remove it from others
+	$("#all-prog-tab").addClass("btn-rounded-white-select");
+	$("#data-forecasts-tab").removeClass("btn-rounded-white-select");
+	$("#analysis-reporting-tab").removeClass("btn-rounded-white-select");
+	$("#smart-economy-tab").removeClass("btn-rounded-white-select");
+  });
+  
+});
+
+$("#navbar-btn-icon").click(function() {
+  $("span.navbar-toggler-icon").toggleClass('clicked');
+});
